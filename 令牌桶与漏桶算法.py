229596f629ff7a9218a1b844cd387ead8519bbc8
cary.zhou <kaichuangluo@gmail.com>
2019-07-22 09:58:10 +0800

    Update 令牌桶与漏桶算法.py

--- 令牌桶与漏桶算法.py
+++ 令牌桶与漏桶算法.py
@@ -9,20 +9,20 @@ class TokenBucket(object):
         self.last_time=time.time()
 
     def take(self,amount):
-        '''从桶内取出令牌,令牌数量不够则返回False'''
+        '''从桶内取出令牌,不够返回False'''
         cur_time=time.time()
         increment=(cur_time-self.last_time)*self.rate
         #剩余令牌+新产生的令牌不得超过令牌桶容量
         self.surplus=min(increment+self.surplus,self.capacity)
         if amount>self.surplus:
             return False
+        #减去拿走的令牌数量
         self.last_time=cur_time
-        #减去拿走的令牌数量等于剩下的令牌数量
         self.surplus-=amount
         return True
 
-    def size(self):
-        '''返回桶内剩余令牌数量'''
+    def get_free(self):
+        '''返回可取的令牌数量'''
         cur_time=time.time()
         increment=(cur_time-self.last_time)*self.rate
         self.surplus=min(increment+self.surplus,self.capacity)
@@ -31,26 +31,38 @@ class TokenBucket(object):
 
 class SpeedTester(object):
     '''漏桶算法,来自$$R'''
-    def __init__(self,speed=0):
-        self.speed=speed*1024       #桶容量
-        self.sumlen=0               #剩余水量
+    def __init__(self,capacity=0):
+        self.capacity=capacity*1024 #桶容量
+        self.volume=0               #剩余水量
         self.last_time=time.time()  #上次漏水时间
 
     def add(self,datalen):
         '''往桶内注水'''
-        if self.speed>0:
+        if self.capacity>0:
             #注水
-            self.sumlen+=datalen
+            self.volume+=datalen
 
     def isExceed(self):
         '''判断桶溢出'''
-        if self.speed>0:
-            cut_t=time.time()
+        if self.capacity>0:
+            cut_time=time.time()
             #漏水
-            self.sumlen-=(cut_t-self.last_time)*self.speed
-            if self.sumlen<0:
-                self.sumlen=0
-            self.last_time=cut_t
+            self.volume-=(cut_time-self.last_time)*self.capacity
+            if self.volume<0:
+                self.volume=0
+            self.last_time=cut_time
             #判断桶溢出
-            return self.sumlen>=self.speed
+            return self.volume>=self.capacity
         return False
+
+    def get_free(self):
+        '''获取空闲容量'''
+        if self.capacity>0:
+            cut_time=time.time()
+            self.volume-=(cut_time-self.last_time)*self.capacity
+            if self.volume<0:
+                self.volume=0
+            self.last_time=cut_time
+            #判断桶溢出
+            return max(self.capacity-self.volume,0)
+        return 1
