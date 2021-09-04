29b30473a5ed9db57b2fe43382df5b431bc1baaf
cary.zhou <kaichuangluo@gmail.com>
2021-09-04 15:05:23 +0800

    Add files via upload

--- route.c
+++ route.c
@@ -11,125 +11,80 @@
 #include <net/if.h>       
 #include <netdb.h>
 
-#define RTACTION_ADD 1   
-#define RTACTION_DEL 2   
-
-void usage();
-int inet_setroute(int action, char **args);
-
-int main(int argc, char **argv) {
-	int action = 0;
-	if(argc < 5) {
-		usage();
-		return -1;
-	}
-	if(strcmp(argv[1], "-A")) {
-		usage();
-		return -1;
-	}
-	if(!strcmp(argv[3], "add")) {
-		action = RTACTION_ADD;
-	}
-	if(!strcmp(argv[3], "del")) {
-		action = RTACTION_DEL;
-	}
-	if(!strcmp(argv[2], "inet")) { /* add or del */
-		inet_setroute(action, argv+4);
-	}
-	return 0;
-}
-
-void usage() {
-	printf("IPv4 Command: route -A inet add/del -net/-host TARGET netmask NETMASK gw GETWAY dev DEVICE mtu MTU\n");
-	return ;
-}
-
-int inet_setroute(int action, char **args) {
+int inet_setroute(char *args[]) {
+	int op = 1; //add
 	struct rtentry route;
-	char target[128] = {0};
+	char dstaddr[128] = {0};
 	char gateway[128] = {0};
 	char netmask[128] = {0};
 	struct sockaddr_in *addr;
-	int skfd;
 	memset((char*)&route, 0x00, sizeof(route));
 	route.rt_flags = RTF_UP;
-	args++;
 	while(args) {
+		args++;
 		if(*args == NULL) {
 			break;
 		}
-		if(!strcmp(*args, "-net")) {
-			args++;
-			strcpy(target, *args);
-			addr = (struct sockaddr_in*)&route.rt_dst;
-			addr->sin_family = AF_INET;
-			addr->sin_addr.s_addr = inet_addr(target);
-			args++;
+		if(!strcmp(*args, "del")) {
+			op = 0; //del
 			continue;
-		} else if(!strcmp(*args, "-host")) {
+		}
+		if(!strcmp(*args, "net")) {
 			args++;
-			strcpy(target, *args);
+			printf("dstaddr:%s\n", *args);
+			strcpy(dstaddr, *args);
 			addr = (struct sockaddr_in*)&route.rt_dst;
 			addr->sin_family = AF_INET;
-			addr->sin_addr.s_addr = inet_addr(target);
+			addr->sin_addr.s_addr = inet_addr(dstaddr);
+			continue;
+		} else if(!strcmp(*args, "host")) {
+			args++;
+			strcpy(dstaddr, *args);
+			addr = (struct sockaddr_in*)&route.rt_dst;
+			addr->sin_family = AF_INET;
+			addr->sin_addr.s_addr = inet_addr(dstaddr);
 			route.rt_flags |= RTF_HOST;
-			args++;
 			continue;
-		} else {
-			usage();
-			return -1;
 		}
 		if(!strcmp(*args, "netmask")) {
 			args++;
+			printf("netmask:%s\n", *args);
 			strcpy(netmask, *args);
 			addr = (struct sockaddr_in*) &route.rt_genmask;
 			addr->sin_family = AF_INET;
 			addr->sin_addr.s_addr = inet_addr(netmask);
-			args++;
 			continue;
 		}
-		if(!strcmp(*args, "gw") || !strcmp(*args, "gateway")) {
+		if(!strcmp(*args, "gateway")) {
 			args++;
+			printf("gateway:%s\n", *args);
 			strcpy(gateway, *args);
 			addr = (struct sockaddr_in*) &route.rt_gateway;
 			addr->sin_family = AF_INET;
 			addr->sin_addr.s_addr = inet_addr(gateway);
 			route.rt_flags |= RTF_GATEWAY;
-			args++;
 			continue;
 		}
-		if(!strcmp(*args, "device") || !strcmp(*args, "dev")) {
+		if(!strcmp(*args, "dev")) {
 			args++;
 			route.rt_dev = *args;
-			args++;
-			continue;
-		}
-		if(!strcmp(*args, "mtu")) {
-			args++;
-			route.rt_flags |= RTF_MTU;
-			route.rt_mtu = atoi(*args);
-			args++;
 			continue;
 		}
 	}
-	skfd = socket(AF_INET, SOCK_DGRAM, 0);
+	int skfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(skfd < 0) {
 		perror("socket");
 		return -1;
 	}
-	if(action == RTACTION_DEL) {
-		if(ioctl(skfd, SIOCDELRT, &route) < 0) {
-			perror("SIOCDELRT");
-			close(skfd);
-			return -1;
-		}
-	} else {
-		if(ioctl(skfd, SIOCADDRT, &route) < 0) {
-			perror("SIOCADDRT");
-			close(skfd);
-			return -1;
-		}
+	if(ioctl(skfd, op>0?SIOCADDRT:SIOCDELRT, &route) < 0) {
+		perror("ioctl");
+		close(skfd);
+		return -1;
 	}
-	(void) close(skfd);
+	close(skfd);
 	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	return inet_setroute(argv);
 }
